1cfa4e598da3128eaf1171cf3c4f4b44253e87f3
40pyd40 <40pyd40@gmail.com>
2019-10-05 17:08:23 +0300

    Entity Framework Core Relationships added

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -21,23 +23,26 @@ namespace Application.Activities
             public string Venue { get; set; }
         }
 
-        public class CommandValidator: AbstractValidator<Command>{
+        public class CommandValidator : AbstractValidator<Command>
+        {
             public CommandValidator()
             {
-                RuleFor(x=>x.Title).NotEmpty();
-                RuleFor(x=>x.Description).NotEmpty();
-                RuleFor(x=>x.Category).NotEmpty();
-                RuleFor(x=>x.Date).NotEmpty();
-                RuleFor(x=>x.City).NotEmpty();
-                RuleFor(x=>x.Venue).NotEmpty();
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
             }
         }
 
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
@@ -54,10 +59,22 @@ namespace Application.Activities
                     Venue = request.Venue
                 };
 
+                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUserName());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendee);
                 _context.Activities.Add(activity);
+
                 var success = await _context.SaveChangesAsync() > 0;
 
-                if(success) return Unit.Value;
+                if (success) return Unit.Value;
 
                 throw new Exception("Problem saving changes");
             }
